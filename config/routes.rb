5ba2ce42a6d91a697193384922ee69aaae231fd5
jose david lombana <jose4125@gmail.com>
2013-06-12 23:31:06 -0500

    routes path lables - projects admin - foundation css

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Portafolio::Application.routes.draw do
-  resources :projects
+  get "welocme/index"
+
+  #resources :projects
 
 
-  resources :labels
+  #resources :labels
 
 
   # The priority is based upon order of creation:
@@ -56,7 +58,7 @@ Portafolio::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
