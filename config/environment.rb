0471bb48938d72664f2a246b242e0c3d47b05d99
jose david lombana <jose4125@gmail.com>
2013-06-14 10:07:13 -0500

    rakefile change

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-if ENV['RAILS_ENV'] == "production"
-    ActiveSupport::Deprecation.silenced = true
-end
+ActiveSupport::Deprecation.silenced = true
 
 # Initialize the rails application
 Portafolio::Application.initialize!
