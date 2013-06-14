b7371d86d9c03d20c2a6f43137e2ab482a0bcffc
jose david lombana <jose4125@gmail.com>
2013-06-14 09:36:17 -0500

    cambio en enviroment.rb deprecation silence true

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+if ENV['RAILS_ENV'] == "production"
+    ActiveSupport::Deprecation.silenced = true
+end
+
 # Initialize the rails application
 Portafolio::Application.initialize!
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,3 @@
-::ActiveSupport::Deprecation.silenced = true
 Portafolio::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
