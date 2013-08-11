0f4de5cb17e8db6c66379aeba8199c8ddb082e84
jose david lombana <jose4125@gmail.com>
2013-08-11 16:53:57 -0500

    add year to projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,18 +2,23 @@
 #
 # Table name: projects
 #
-#  id          :integer          not null, primary key
-#  name        :string(255)
-#  description :text
-#  imagen      :string(255)
-#  desc_corta  :string(255)
-#  url         :string(255)
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
+#  id                 :integer          not null, primary key
+#  name               :string(255)
+#  description        :text
+#  imagen             :string(255)
+#  desc_corta         :string(255)
+#  url                :string(255)
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  photo_file_name    :string(255)
+#  photo_content_type :string(255)
+#  photo_file_size    :integer
+#  photo_updated_at   :datetime
+#  year               :integer
 #
 
 class Project < ActiveRecord::Base
-  attr_accessible :desc_corta, :description, :imagen, :name, :url, :skill_ids, :photo
+  attr_accessible :desc_corta, :description, :imagen, :name, :url, :skill_ids, :photo, :year
   has_and_belongs_to_many :skills
   has_attached_file :photo, style: { medium: '300X300>'},
                     :default_url => '/assets/default.jpg',
--- test/unit/project_test.rb
+++ test/unit/project_test.rb
@@ -2,14 +2,19 @@
 #
 # Table name: projects
 #
-#  id          :integer          not null, primary key
-#  name        :string(255)
-#  description :text
-#  imagen      :string(255)
-#  desc_corta  :string(255)
-#  url         :string(255)
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
+#  id                 :integer          not null, primary key
+#  name               :string(255)
+#  description        :text
+#  imagen             :string(255)
+#  desc_corta         :string(255)
+#  url                :string(255)
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  photo_file_name    :string(255)
+#  photo_content_type :string(255)
+#  photo_file_size    :integer
+#  photo_updated_at   :datetime
+#  year               :integer
 #
 
 require 'test_helper'
